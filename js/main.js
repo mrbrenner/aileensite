b58204b941057987a2f92eafb569ab3669ef57c2
mrbrenner <brenm566@newschool.edu>
2017-06-20 16:14:51 +0200

    final site files added

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$ (function () {
+
+	console.log("ready");
+
+	var state = false;
+
+	// mobile activate navbar on click
+
+	$("#dropActivate").click( function(){
+		
+		state = !state;
+		console.log(state);
+		
+		//if = odd click (open), else = even click (closed)
+
+		if (state==true) {
+			console.log("show");
+			$(".drop").show();
+			$("#contactButton").show();
+			$("#dropActivate a").css("color", "#808080");
+		} else {
+			console.log("hide");
+			$(".drop").hide();
+			$("#dropActivate a").css("color", "#A9A9A9")
+			$("#contactButton").hide();
+		}
+	});
+
+
+});
